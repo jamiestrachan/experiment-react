2d97ada696926b7cf72ed19b5f1b042e7fb706a9
Jamie Strachan <jamie.strachan@cbc.ca>
2015-05-26 23:40:17 -0400

    trying to get content updating working

--- app.jsx
+++ app.jsx
@@ -19,11 +19,37 @@ var Card = React.createClass({
 });
 
 var Feed = React.createClass({
+  loadContent: function () {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({content: data.contentlist.contentitems});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function () {
+    return {content: []};
+  },
+  componentDidMount: function() {
+    this.loadContent();
+    setInterval(this.loadContent, this.props.pollInterval);
+  },
   render: function() {
     var cards = [];
     var contentBetweenAds = 6;
-    this.props.content.forEach(function(contentItem) {
+    var sinceLastAd = 0;
+    this.state.content.forEach(function(contentItem) {
+      if (sinceLastAd >= contentBetweenAds) {
+        cards.push(<Ad />);
+        sinceLastAd = 0;
+      }
       cards.push(<Card contentItem={contentItem} key={contentItem.id} />);
+      sinceLastAd++;
     });
     return (
       <div className="feed">
