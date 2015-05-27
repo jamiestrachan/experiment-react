fbd0ee473397843e61fcb84d7595aec8704afba8
Jamie Strachan <jamie.strachan@cbc.ca>
2015-05-26 22:47:04 -0400

    moving jsx to separate file

---
+++ app.jsx
@@ -0,0 +1,12 @@
+var Feed = React.createClass({
+  render: function() {
+    <div className="feed">
+      The Feed
+    </div>
+  }
+});
+      
+React.render(
+  <Feed />,
+  document.getElementById('content')
+);
